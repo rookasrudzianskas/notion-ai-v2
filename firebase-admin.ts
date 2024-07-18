a1ca9d79aa169e7dd43153ace36440dab86c98a5
rokas <rokas@rokas.com>
2024-07-18 11:13:47 +0300

    Add default inspection profile and handle firebase admin setup

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -1,2 +1,15 @@
-import { initializeApp } from "firebase-admin/app";
+import { initializeApp, getApps, App, getApp, cert } from "firebase-admin/app";
 
+const serviceKey = require("@/service-key.json");
+
+let app: App;
+
+if (getApps().length === 0) {
+  app = initializeApp({
+    credential: cert(serviceKey),
+  });
+} else {
+  app = getApp();
+}
+
+export {app};
